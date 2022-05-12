019ae8ed6bd012b20e8b9ca78f2048814c6b6f28
xnguyenvanlinh <xnguyenvanlinh@gmail.com>
2022-05-12 21:49:48 +0700

    feat : props

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AdminComponent } from './pages/admin/admin.component';
+import { HelloComponent } from './hello/hello.component';
+import { NewComponent } from './new/new.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AdminComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent, AdminComponent, HelloComponent, NewComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/new/new.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new',
+  templateUrl: './new.component.html',
+  styleUrls: ['./new.component.css'],
+})
+export class NewComponent implements OnInit {
+  @Input() test!: string;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
