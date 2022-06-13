ff0e125eca4acf6c898ecfda9fe3fbb0e2af2733
xnguyenvanlinh <xnguyenvanlinh@gmail.com>
2022-06-14 00:03:16 +0700

    feat : add action convert Date to string

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -6,3 +6,12 @@ export default function isAdmin() {
 export function isAuth() {
   return localStorage.getItem('user');
 }
+export function _date(date: number) {
+  var result = '';
+  var d = new Date(date);
+  result += d.getDate() + '-' + (d.getMonth() + 1) + '-' + d.getFullYear();
+  if (!date) {
+    return '';
+  }
+  return result;
+}
